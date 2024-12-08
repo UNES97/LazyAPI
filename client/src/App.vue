6f94f184ff17109104e8940c85a5786eb681eb3d
Younes Noufair <48910083+UNES97@users.noreply.github.com>
2024-12-08 22:02:33 +0100

    .

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,12 @@
 <script>
+import MainMenu from "./components/MainMenu.vue";
+export default {
+    components: { MainMenu },
+}
 </script>
 
 <template>
-
+    <MainMenu></MainMenu>
 </template>
 
 <style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,23 @@
 import { createApp } from 'vue'
 import './style.css'
 import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
 import App from './App.vue'
+import Menubar from 'primevue/menubar';
+import Tag from 'primevue/tag';
 
 
 const app = createApp(App);
-app.use(PrimeVue);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            prefix: 'p',
+            darkModeSelector: 'system',
+            cssLayer: false
+        }
+    }
+});
+app.component('Menubar', Menubar);
+app.component('Tag', Tag);
 app.mount('#app');
