002111ad6c3223745ad9ef7274ae9a79a529bd05
Younes Noufair <48910083+UNES97@users.noreply.github.com>
2024-12-08 21:32:50 +0100

    Clean Template :100:

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
+import PrimeVue from 'primevue/config';
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+const app = createApp(App);
+app.use(PrimeVue);
+app.mount('#app');
